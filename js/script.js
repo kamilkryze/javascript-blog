ed36850a6cac512c2f0f51c486dc13e79798cab8
Kamil Kryze <kryzekamil@gmail.com>
2022-08-01 23:00:35 +0200

    error

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
   console.log('show', event)
@@ -15,12 +16,10 @@ function titleClickHandler(event){
   }
 
   /* add class 'active' to the clicked link */
-
   clickedElement.classList.add('active');
   console.log('clickedElement:', clickedElement);
 
   /* remove class 'active' from all articles */
-
   const activeArticles = document.querySelectorAll('.post');
 
   for(let activeArticle of activeArticles){
@@ -28,10 +27,21 @@ function titleClickHandler(event){
   }
 
   /* get 'href' attribute from the clicked link */
+  const articleSelector = href;
+  console.log('show', articleSelector);
+  clickedElement.getAttribute('href');
 
   /* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector('.titles href');
+  console.log('show', targetArticle);
+/* ERROR - script.js:30 Uncaught ReferenceError: href is not defined
+at HTMLAnchorElement.titleClickHandler (script.js:30:27) */
+  
 
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
+  console.log('clickedElement:', targetArticle);
+  
 }
 
 const links = document.querySelectorAll('.titles a');
